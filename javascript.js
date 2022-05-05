26f99fbbc87b80e15e149f2897ace546384d1700
AtilaIvich <102477378+AtilaIvich@users.noreply.github.com>
2022-05-05 18:37:24 -0300

    Code improvement and bug fixing

--- javascript.js
+++ javascript.js
@@ -8,6 +8,12 @@ const q7 = document.getElementById('q7');
 const q8 = document.getElementById('q8');
 const q9 = document.getElementById('q9');
 
+const outpName1 = document.getElementById('outputName1');
+const outpName2 = document.getElementById('outputName2');
+
+const outpScore1 = document.getElementById('outputScore1');
+const outpScore2 = document.getElementById('outputScore2');
+
 q1.addEventListener('click', () => play(1));
 q2.addEventListener('click', () => play(2));
 q3.addEventListener('click', () => play(3));
@@ -17,6 +23,13 @@ q6.addEventListener('click', () => play(6));
 q7.addEventListener('click', () => play(7));
 q8.addEventListener('click', () => play(8));
 q9.addEventListener('click', () => play(9));
+
+var name1 = prompt('Player 1 nickname').toUpperCase();
+outpName1.innerText = `${name1} SCORE:`
+
+var name2 = prompt('Player 2 nickname').toUpperCase();
+outpName2.innerText = `${name2} SCORE:`
+
 
 var turn = 1;
 
@@ -35,16 +48,6 @@ var c3;
 var score1 = 0;
 var score2 = 0;
 
-var name1 = prompt('Player 1 nickname');
-name1 = name1.toUpperCase();
-document.getElementById('output1').innerHTML = `${name1} SCORE: <span id="outputScore1">0</span>`
-
-var name2 = prompt('Player 2 nickname');
-name2 = name2.toUpperCase();
-document.getElementById('output2').innerHTML = `${name2} SCORE: <span id="outputScore1">0</span>`
-
-const outp1 = document.getElementById('outputScore1');
-const outp2 = document.getElementById('outputScore2');
 
 function play(n) {
     switch (n) {
@@ -158,377 +161,107 @@ function play(n) {
     }
 
     if (a1 == 1 && a2 == 1 && a3 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
     if (b1 == 1 && b2 == 1 && b3 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
     if (c1 == 1 && c2 == 1 && c3 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
 
     if (a1 == 1 && b1 == 1 && c1 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
     if (a2 == 1 && b2 == 1 && c2 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
     if (a3 == 1 && b3 == 1 && c3 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
 
     if (a1 == 1 && b2 == 1 && c3 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
     if (a3 == 1 && b2 == 1 && c1 == 1) {
-        score1 += 1;
-        outp1.innerText = score1;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win1();
+        restart();
     }
 
 
     if (a1 == 2 && a2 == 2 && a3 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
     if (b1 == 2 && b2 == 2 && b3 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
     if (c1 == 2 && c2 == 2 && c3 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
 
     if (a1 == 2 && b1 == 2 && c1 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
     if (a2 == 2 && b2 == 2 && c2 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
     if (a3 == 2 && b3 == 2 && c3 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
 
     if (a1 == 2 && b2 == 2 && c3 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
     if (a3 == 2 && b2 == 2 && c1 == 2) {
-        score2 += 1;
-        outp2.innerText = score2;
-        
-        a1 = 0;
-        a2 = 0;
-        a3 = 0;
-        b1 = 0;
-        b2 = 0;
-        b3 = 0;
-        c1 = 0;
-        c2 = 0;
-        c3 = 0;
-        q1.style.backgroundImage = "inherit";
-        q2.style.backgroundImage = "inherit";
-        q3.style.backgroundImage = "inherit";
-        q4.style.backgroundImage = "inherit";
-        q5.style.backgroundImage = "inherit";
-        q6.style.backgroundImage = "inherit";
-        q7.style.backgroundImage = "inherit";
-        q8.style.backgroundImage = "inherit";
-        q9.style.backgroundImage = "inherit";
+        win2();
+        restart();
     }
+}
+
+function win1() {
+    score1 += 1;
+    outpScore1.innerText = score1;
+}
+
+function win2() {
+    score2 += 1;
+    outpScore2.innerText = score2;
+}
+
+function restart() {
+    a1 = 0;
+    a2 = 0;
+    a3 = 0;
+    
+    b1 = 0;
+    b2 = 0;
+    b3 = 0;
+
+    c1 = 0;
+    c2 = 0;
+    c3 = 0;
+
+    q1.style.backgroundImage = "inherit";
+    q2.style.backgroundImage = "inherit";
+    q3.style.backgroundImage = "inherit";
+    q4.style.backgroundImage = "inherit";
+    q5.style.backgroundImage = "inherit";
+    q6.style.backgroundImage = "inherit";
+    q7.style.backgroundImage = "inherit";
+    q8.style.backgroundImage = "inherit";
+    q9.style.backgroundImage = "inherit";
 }
